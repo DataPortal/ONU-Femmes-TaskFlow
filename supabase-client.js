7c6e64eb28edf4962afbacc2531022e03809d97a
UNWOMEN <hypolite.reoutar@unwomen.org>
2026-03-19 19:03:37 +0100

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://ptdqwggwwougrmkbsuvl.supabase.co";
+const SUPABASE_KEY = "sb_publishable_xwdi8H9UizW5CKMNVAQHxg_hz_yVoIz";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
